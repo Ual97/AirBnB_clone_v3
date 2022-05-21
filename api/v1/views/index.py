0d588673b05a99d66c0f4f3e27a51913b7eb56c1
sl4 <3940@holbertonschool.com>
2022-05-21 14:16:00 -0700

    added ex 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,21 @@
 """index module"""
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status')
 def myssatus():
     """sends json status"""
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats')
+def count_objects():
+    """retrieves the number of each object by type"""
+    return jsonify({'amenities': storage.count('Amenity'),
+                    'cities': storage.count('City'),
+                    'places': storage.count('Place'),
+                    'reviews': storage.count('Review'),
+                    'states': storage.count('State'),
+                    'users': storage.count('User')})
