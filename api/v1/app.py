2ebd70366157c7b42198ca70438ae2a0d5761c10
sl4 <3940@holbertonschool.com>
2022-05-21 14:42:38 -0700

    added ex 6

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """app module"""
 from os import environ
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -14,6 +14,12 @@ app.register_blueprint(app_views)
 def teardwn(exception):
     """teardown context"""
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """page for 404 error"""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == '__main__':
