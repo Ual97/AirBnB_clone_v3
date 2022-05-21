9320e537a89dd16706c8abc5765287b19b97440f
sl4 <3940@holbertonschool.com>
2022-05-21 13:56:00 -0700

    docstrings

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+"""index module"""
 from flask import jsonify
 from api.v1.views import app_views
 
 
 @app_views.route('/status')
-def sts():
+def myssatus():
     """sends json status"""
     return jsonify({'status': 'OK'})
