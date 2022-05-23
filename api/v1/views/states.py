5380c66909b3d9c54900d68c76fe37984f33cc90
Mateo Victorica <3951@holbertonschool.com>
2022-05-23 16:05:43 -0300

    added GET method to /states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,7 +6,7 @@ from models import storage
 from models.state import State
 
 
-@app_views.route('/states', methods=['POST'])
+@app_views.route('/states', methods=['POST'], methods=['GET'])
 def view_states():
     """sends list of state objects as dicts"""
     if (request.method == 'GET'):
